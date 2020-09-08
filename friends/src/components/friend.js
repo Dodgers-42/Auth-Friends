98a80dafd565bc9d682d84114272be9bddff3172
Dodger-42 <kevin.bing.barber@gmail.com>
2020-09-08 15:47:48 -0700

    working threw friends

--- friends/src/components/friend.js
+++ friends/src/components/friend.js
@@ -20,23 +20,24 @@ const Friend = (props) => {
         setEditing(false);
     }
     return (
-        // <div>
-        //     <div>
-        //         {editing ?(
-        //         <input
-        //             onChange={onChange}type="text" name="name"value={edits.name}placeholder="name"/>
-        //         <input
-        //         onChange={onChange}type="number" name="age"value={edits.age}placeholder="age"/>
-        //         <input
-        //         onChange={onChange}type="email" name="email"value={edits.email}placeholder="email"/>
-        //     </div>
-        //     <div>
-        //         <button onClick={()=> setEditing(false)}>Cancel</button>
-        //         <button onClick={()=> submitEdits(props.friend.id)}Submit Edits></button>
-        //     </div>
-        // </div>
+        <div>
+            <div>
+                {editing ?(
+                <input
+                    onChange={onChange}type="text" name="name"value={edits.name}placeholder="name"/>
+                <input
+                    onChange={onChange}type="number" name="age"value={edits.age}placeholder="age"/>
+                <input
+                    onChange={onChange}type="email" name="email"value={edits.email}placeholder="email"/>
+            </div>
+            <div>
+                <button onClick={()=> setEditing(false)}>Cancel</button>
+                <button onClick={()=> submitEdits(props.friend.id)}Submit Edits></button>
+            </div>
+        </div>
     );
 }
 
 )}
+
 export default Friend;
--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -6,5 +6,65 @@ import Friend from "./Friend";
 const axios = axiosWithAuth();
 
 const Friends = (props) => {
-    const []
+    const [data, setData] = useState({
+        name: "",
+        age: "",
+        email: ""
+    })
+
+    const onChange = e => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        axios.post("http://localhost:5000/api/friends", data)
+        .then(({data})=> {
+            props.setFriends(data);
+            setData({
+                name: "",
+                age: "",
+                email: ""
+            });
+        })
+        .catch(err=>{
+            console.log(err);
+            console.log(props.setFriends);
+        });
+    }
+
+    const deleteFriend = id => {
+        axios.delete(`http://localhost:5000/api/friends/${id}`)
+        .then(({data})=>{
+            props.setFriends(data);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+
+    const updateFriend = (id, data) => {
+        axios.delete(`http://localhost:5000/api/friends/${id}`, data)
+        .then(({data})=>{
+            props.setFriends(data);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        axios.delete("http://localhost:5000/api/friends")
+        .then(({data})=>{
+            props.setFriends(data);
+        })
+        .catch(err=>{
+            console.log(props.setFriends);
+        
+        });
+    }, []);
+
 }
